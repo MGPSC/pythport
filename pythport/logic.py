aa5a6668b4e41bfa146eb2d74abdafd456fde44a
Joe Streifel <josephstreifel@gmail.com>
2021-12-28 15:33:00 -0800

    sets up ability to create and store master hash
    Co-Authored-By: Brannon Starnes <79380625+brannonstarnes@users.noreply.github.com>

--- pythport/logic.py
+++ pythport/logic.py
@@ -2,7 +2,7 @@
 # import aes
 # import sys
 # from pythport.aes import AesEncrypt
-# from pythport.bcrypt import BcryptEncrypt
+from pythport.bcrypt import BcryptEncrypt
 # from base64 import b64encode
 # from Crypto.Hash import SHA256
 # from Crypto.Protocol.KDF import bcrypt
@@ -10,13 +10,29 @@
 # from Crypto.Cipher import AES
 import json
 
-master = {}
-master['hash'] = [bcrypt_hash] 
+# master['hash'] = [bcrypt_hash] 
 
 
-def master_to_json(bcrypt_hash):
-    
-    with open('../assets/master.json', "w") as f:
-        json.dumps()
+def create_master_hash(master_dict):
+    passphrase = input("Please enter your desired password \n> ")
+    bc = BcryptEncrypt(passphrase)
+    hash = bc.cook_hash()
+    master_dict["master"]["hash"] = hash.decode()
+    with open('assets/master.json', "w") as f:
+        f.write(json.dumps(master_dict))
 
-    
+
+def master_to_json():
+    with open('assets/master.json', "r") as f:
+        master_json = f.read()
+        print("M JSON: ", master_json)
+        master_dict = json.loads(master_json)
+        print("dict:", master_dict)
+
+    if master_dict["master"]["hash"] is None:
+        create_master_hash(master_dict)
+        
+    # with open('../assets/master.json', "w") as f:
+    #     json.dumps()
+
+master_to_json()    
