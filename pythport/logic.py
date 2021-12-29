d12336ae1b3d2fdd413ecf6a9198da00ac057d74
Joe Streifel <josephstreifel@gmail.com>
2021-12-28 16:22:28 -0800

    adds ability to verify master password
    Co-Authored-By: Brannon Starnes <79380625+brannonstarnes@users.noreply.github.com>

--- pythport/logic.py
+++ pythport/logic.py
@@ -21,18 +21,33 @@ def create_master_hash(master_dict):
     with open('assets/master.json', "w") as f:
         f.write(json.dumps(master_dict))
 
+def validate_master_pwd(master_dict):
+    attempt = input("Please enter your password:\n> ")
+    master_hash = master_dict["master"]["hash"].encode()
+
+    validated = BcryptEncrypt.validate_pwd(attempt, master_hash)
+    print("Validated:", validated)
 
 def master_to_json():
     with open('assets/master.json', "r") as f:
         master_json = f.read()
-        print("M JSON: ", master_json)
         master_dict = json.loads(master_json)
-        print("dict:", master_dict)
 
     if master_dict["master"]["hash"] is None:
         create_master_hash(master_dict)
+
+    else:
+        validate_master_pwd(master_dict)
         
     # with open('../assets/master.json', "w") as f:
     #     json.dumps()
 
-master_to_json()    
+       
+       
+
+master_to_json()
+
+# validate_master_pwd("potato123")
+
+#{"master": {"hash": "$2a$14$FqQPPZGIOuc8tnLWprUjHu/9fdodsO0Pv1E19gYpjpffCosnrh.BC"}}
+#{"master": {"hash": "$2a$14$9v.mWeDdTF7VjNXQQzlXregwcbjuJ.hOWjleEf2hJIBErdeYzwK.m"}}
