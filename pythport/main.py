91f5666495362cdd68a9a8f52b870627d3c53c92
Joe Streifel <josephstreifel@gmail.com>
2021-12-30 14:23:30 -0800

    builds out main, begins landing page
    begins building out main class, landing page class.
    
    Ability to create new password on first run complete
    
    Co-Authored-By: Brannon Starnes <79380625+brannonstarnes@users.noreply.github.com>
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>

--- pythport/main.py
+++ pythport/main.py
@@ -2,12 +2,31 @@ import tkinter as tk
 from tkinter import ttk
 
 from pythport.master_manager import MasterManager
-from pythport.aes import AesEncrypt
-from pythport.interface import login_screen
-    
-mm = MasterManager()
+from pythport.pass_manager import PassManager
+from pythport.login_screen import LoginPage
+from pythport.landing_page import LandingPage
+ 
+class PythPortMain():
+    def __init__(self):
+        self.mm = MasterManager()
+        self.root = tk.Tk()
+        self.root.title("PythPort")
+        self.window = LoginPage(self.root, self.mm)
+        self.window.render_self()
+        self.pm = None
+        self.root.mainloop()
 
-root = tk.Tk()
-root.title("PythPort")
-login_screen(root, mm)
-root.mainloop()
+    def set_main_pm(self, password):
+        self.pm = PassManager(password)
+
+    def show_login_page(self):
+        self.window = LoginPage(self.root, self.mm)
+        #clear page/render page
+        pass
+
+    def show_landing_page(self):
+        self.window = LandingPage(self.root, self.pm)
+        
+        pass
+
+app = PythPortMain()
