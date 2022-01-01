d6dd3541033bd4ec77dd7b5581b08eb59b49f770
Joe Streifel <josephstreifel@gmail.com>
2022-01-01 15:23:33 -0800

    cleans up prints and code
    Co-Authored-By: Jordan Folk <89801472+folksmash@users.noreply.github.com>
    Co-Authored-By: Brannon Starnes <79380625+brannonstarnes@users.noreply.github.com>

--- pythport/main.py
+++ pythport/main.py
@@ -6,23 +6,16 @@ from pythport.pass_manager import PassManager
 import json
 from random import randint
 
-# from pythport.login_screen import LoginPage
-# from pythport.landing_page import LandingPage
-
 #####
-## MAIN LOGIC
+## MAIN APP LOGIC / STATE
 #####
 
 class PythPortMain(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        # self.geometry('800x300')
         self.window = None
         self.mm = MasterManager()
-        # self.root = tk.Tk()
         self.title("PythPort")
-        # self.window = LoginPage(self, self.mm)
-        # self.window.render_self()
         self.pm = None
         self.switch_frame(LoginPage)
 
@@ -36,21 +29,9 @@ class PythPortMain(tk.Tk):
             self.window.destroy()
         self.window = new_frame
         self.window.render_self()
-        # self.window.pack()
-
-    # def show_login_page(self):
-    #     self.window = LoginPage(self, self.mm)
-    #     self.window.render_self()
-    #     #clear page/render page
-
-    # def show_landing_page(self):
-    #     self.window = LandingPage(self, self.pm)
         
-    #     pass
-
-
 #####
-## LOGIN PAGE LOGIC
+## LOGIN PAGE
 #####
 
 class LoginPage(tk.Frame):
@@ -67,7 +48,6 @@ class LoginPage(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pw_entry = tk.StringVar()
         self.main = master
-        # self.content = ttk.Frame(self.root)
 
     def submit(self):
         password = self.pw_entry.get()
@@ -87,38 +67,27 @@ class LoginPage(tk.Frame):
         else:
             messagebox.showerror(title="Incorrect Password", message="Incorrect password!\nPlease try again!")
 
-    # @staticmethod
-    # def clear_all(element):
-    #     for item in element.winfo_children():
-    #         item.destroy()
-    #     element.destroy()
 
     def render_self(self):
         pass_hash = self.main.mm.master_dict["master"]["hash"]
         self.pw_entry = tk.StringVar()
-        # self.clear_all(self)
-        # self = ttk.Frame(self.root)
         self.grid(column = 0, row = 0, columnspan = 4, rowspan = 4, padx=(50, 50), pady=(10, 50))
 
-        # Common Parts
         warn_label = ttk.Label(self, text="DO NOT FORGET THIS PASSWORD.\nWithout your password, you data will be lost forever.", justify = "center")
         pw_label = ttk.Label(self, text="Password:", justify = "right")
 
-        #Parts for no saved login
         if pass_hash is None:  
             pw_entry = ttk.Entry(self, textvariable=self.pw_entry)
             label = ttk.Label(self, text="Welcome to Pythport!\nLet's get you set up.", justify = "center")
             prompt_label = ttk.Label(self, text = 'Please enter your desired password (at least 8 characters).')
             submit_btn = ttk.Button(self, text="Create", command = self.submit)
 
-        #Parts for existing saved login
         if pass_hash:
             pw_entry = ttk.Entry(self, textvariable=self.pw_entry, show="*")
             label = ttk.Label(self, text="Welcome to Pythport!\nLet's go!", justify = "center")
             prompt_label = ttk.Label(self, text = 'Please enter your password.')
             submit_btn = ttk.Button(self, text="Log In", command = self.login)
 
-        #put the parts in a grid
         label.grid(column = 0, row = 0, columnspan = 2, pady=(10,10))
         prompt_label.grid(column = 0, row = 1, columnspan = 2, pady=(10,30))
         pw_label.grid(column=0, row = 2, pady=(0, 10))
@@ -127,7 +96,7 @@ class LoginPage(tk.Frame):
         warn_label.grid(column = 0, row = 4, columnspan = 2, pady=(0,20))
 
 ######
-## LandingPage Logic
+## LANDING PAGE
 ######
 
 class LandingPage(tk.Frame):
@@ -135,12 +104,8 @@ class LandingPage(tk.Frame):
         tk.Frame.__init__(self, master)
         self.main = master
         self.hide = True
-        # self.root = root
-        # self.pm = pm
         self.pw_entry = tk.StringVar()
-        # self.content = ttk.Frame(self.root)
-
-
+   
     def render_self(self):
         self.grid(column = 0, row = 0, columnspan = 4, rowspan = 4, padx=(50, 50), pady=(10, 50))
         tv = ttk.Treeview(self)
@@ -168,8 +133,6 @@ class LandingPage(tk.Frame):
                     tree.insert('', 'end', values=info)
                 elif self.hide == True:
                     tree.insert('', 'end', values=info[:2] + ['*'*8, '*'*8])  
-        render_tree(tv)
-        tv['show'] = 'headings'
      
         def toggle_hide(tv):
 
@@ -179,11 +142,6 @@ class LandingPage(tk.Frame):
             elif self.hide == False:
                 self.hide = True
                 render_tree(tv)    
-            
-                pass
-            # tv.pack(side="top", fill="both", expand=True)
-        # label = tk.Label(self, text="This is page 2")
-        # label.pack(side="top", fill="both", expand=True)
 
         def go_to_update():
             try:
@@ -193,12 +151,11 @@ class LandingPage(tk.Frame):
             except IndexError:
                 messagebox.showerror("No entry selected!", "Please select a login from the list to update/remove.")
 
+        render_tree(tv)
+        tv['show'] = 'headings'
+        
         self.Treeview = tv
-        # self.grid_rowconfigure(0, weight = 1)
-        # self.grid_columnconfigure(0, weight = 1)
         tv.grid(column = 0, row = 1, columnspan=4)
-
-#      BUTTONS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         add_new_btn = ttk.Button(self, text="Add New", command = lambda: self.main.switch_frame(AddNewLogin))
         add_new_btn.grid(column=0, row=0)
@@ -213,6 +170,10 @@ class LandingPage(tk.Frame):
         update_btn = ttk.Button(self, text="Update/Remove", command = go_to_update)
         update_btn.grid(column=3, row= 0)
         
+######
+## ADD NEW LOGIN
+######
+
 class AddNewLogin(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -223,8 +184,6 @@ class AddNewLogin(tk.Frame):
         self.new_pwd = tk.StringVar()
 
     def handle_save(self):
-        #wire up to pm object and add new entry based on state at submit
-        #then go back to landing
         new = [{'name': self.new_name.get(), 'url': self.new_url.get(), 'username': self.new_username.get(), "password": self.new_pwd.get()}]
         self.main.pm.create_login(new)
         self.main.switch_frame(LandingPage)
@@ -246,7 +205,6 @@ class AddNewLogin(tk.Frame):
         create_btn = ttk.Button(self, text="Save", command=self.handle_save)
         cancel_btn = ttk.Button(self, text="Cancel", command = lambda: self.main.switch_frame(LandingPage))
 
-        #TODO: Come back and fix this grid, man
         title_label.grid(column=0, row=0, columnspan=5)
 
         name_label.grid(column=1, row=1, columnspan=2)
@@ -261,6 +219,9 @@ class AddNewLogin(tk.Frame):
         create_btn.grid(column = 3, row=5, pady=(20,20))
         cancel_btn.grid(column=4, row=5, pady=(20,20))
 
+######
+## UPDATE / REMOVE
+######
 
 class UpdateRemove(tk.Frame):
     def __init__(self, master):
@@ -336,10 +297,9 @@ class UpdateRemove(tk.Frame):
         save_btn.grid(row=8, column=2, padx=(30,3))
         cancel_btn.grid(row=8, column=3)
 
-        # self.grid_rowconfigure(0, weight=1)
-        # self.grid_rowconfigure(9, weight=1)
-        # self.grid_columnconfigure(0, weight=1)
-        # self.grid_columnconfigure(4, weight=1)
+######
+## GENERATE PASSWORD
+######
 
 class GenPassword(tk.Frame):
     def __init__(self, master):
@@ -353,8 +313,8 @@ class GenPassword(tk.Frame):
             pw_entry.delete(0, tk.END)
             pw_length = int(my_entry.get())
             my_pass = ''
-      # the reason the randint is 33-126 is because that is the range of ASCII keys that are printable and/or usable as a password
-            for i in range (pw_length):
+
+            for _ in range (pw_length):
                 my_pass += chr(randint(33,126))
             pw_entry.insert(0, my_pass)
 
@@ -362,12 +322,12 @@ class GenPassword(tk.Frame):
         def clipb():
             self.clipboard_clear()
             self.clipboard_append(pw_entry.get())
-            print(pw_entry.get())
 
         lf = tk.LabelFrame(self, text="How Many Characters?")
         lf.pack(pady=20)
 
         my_entry = tk.Entry(lf, font=("Helvetica", 24))
+        my_entry.insert(0, "8")
         my_entry.pack(pady=20, padx=20)
 
         pw_entry = tk.Entry(self, text='', font=("Helvetica", 24), bd=0, bg="#000000")
@@ -381,8 +341,6 @@ class GenPassword(tk.Frame):
 
         back_button = tk.Button(self, text= "Back", command = lambda: self.master.switch_frame(LandingPage))
         back_button.pack(padx= 30, pady= 10)
-        ### TODO: Add back button to return to landing page
-
 
         clip_button = tk.Button(self, text="Copy To Clipboard", command= clipb)
         clip_button.pack(padx=20, pady=10)
--- pythport/pass_manager.py
+++ pythport/pass_manager.py
@@ -16,20 +16,13 @@ class PassManager():
         with open("assets/saved.json", "r") as f:
             saved_json = f.read()
             self.saved_logs = json.loads(saved_json)
-
-    # from GUI inputs: [{name: blah, url: www.blah.com, username: bb@email.com, password: 123abc}]
     
     def create_login(self, login_list):
         new_dict = login_list[0]
         new_dict['username'] = self.crypto.encrypt(new_dict['username'])
         new_dict['password'] = self.crypto.encrypt(new_dict['password'])
 
-        # if self.saved_logs.has_key(new_dict["name"]):
-        #     new_dict["name"] = new_dict["name"] + "(1)"
-
         self.saved_logs[new_dict["name"]] = new_dict
-
-        print("saved logs:", self.saved_logs)
         
         with open('assets/saved.json', "w") as f:
             f.write(json.dumps(self.saved_logs, indent = 4))
@@ -61,6 +54,3 @@ if __name__ == "__main__":
 
     my_passes = PassManager("potato123")
     my_passes.create_login(new)
-
-    print("decrypted version:", my_passes.get_decrypted("spam"))
-    print("encrypted version:", my_passes.get_encrypted("spam"))
