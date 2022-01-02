b11a473832edff8b7f871f2f015ab571f85c090f
Brannon Starnes <brannlee808@gmail.com>
2022-01-01 20:41:45 -0800

    edits landing page column headings, adds several docstrings

--- pythport/main.py
+++ pythport/main.py
@@ -11,6 +11,12 @@ from random import randint
 #####
 
 class PythPortMain(tk.Tk):
+    """
+    Intantiates tkinter GUI components. 
+    #### METHODS
+    - `.set_main_pm(password)` - takes a string, instantiates PassManager as a self.pm.
+    - `.switch_frame(frame_class)` - takes a string, destroys frame and replaces with a new frame.
+    """
     def __init__(self):
         tk.Tk.__init__(self)
         self.window = None
@@ -23,7 +29,6 @@ class PythPortMain(tk.Tk):
         self.pm = PassManager(password)
 
     def switch_frame(self, frame_class):
-        """Destroys current frame and replaces it with a new one."""
         new_frame = frame_class(self)
         if self.window is not None:
             self.window.destroy()
@@ -41,7 +46,7 @@ class LoginPage(tk.Frame):
     #### METHODS
     
     - .submit() - ensures the newly created master password is encrypted and stored in master.json
-    - .login() - 
+    - .login() - evaluates login and redirects to LandingPage
     - .render_self() - renders 'create password' or 'log in' page dependent on whether or not a master password is stored in json.
     """
     def __init__(self, master):
@@ -109,11 +114,11 @@ class LandingPage(tk.Frame):
     def render_self(self):
         self.grid(column = 0, row = 0, columnspan = 4, rowspan = 4, padx=(50, 50), pady=(10, 50))
         tv = ttk.Treeview(self)
-        tv['columns'] = ('Name', 'Username', 'Email', 'Password')
+        tv['columns'] = ('Name', 'URL', 'Email', 'Password')
         tv.heading('Name', text='Name')
         tv.column('Name', anchor='center', width=150)
-        tv.heading('Username', text='Username')
-        tv.column('Username', anchor='center', width=150)
+        tv.heading('URL', text='URL')
+        tv.column('URL', anchor='center', width=150)
         tv.heading('Email', text='Email')
         tv.column('Email', anchor='center', width=200)
         tv.heading('Password', text='Password')
@@ -224,6 +229,7 @@ class AddNewLogin(tk.Frame):
 ######
 
 class UpdateRemove(tk.Frame):
+    """Instantiates the update/remove tkinter frame."""
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.main = master
@@ -302,6 +308,7 @@ class UpdateRemove(tk.Frame):
 ######
 
 class GenPassword(tk.Frame):
+    """Instantiates the random password generator frame in tkinter. """
     def __init__(self, master):
         tk.Frame.__init__(self, master)
     
